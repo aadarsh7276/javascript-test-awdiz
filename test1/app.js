08c93dc72bbe2b7872d2114b2b8b7fec9ba58989
Aadarsh Yadav <152860259+aadarsh7276@users.noreply.github.com>
2024-10-21 13:13:06 +0530

    updates

--- test1/app.js
+++ test1/app.js
@@ -1,150 +1,150 @@
 // print numbers from 10 to 20 with gap of 2 
 
-// for (i = 10 ; i <= 20; i += 2){
-//     console.log(i);
+for (i = 10 ; i <= 20; i += 2){
+    console.log(i);
     
-// }
+}
 
 // print even numbers from 1 to 10 
 
-// for (i = 1 ;i <= 10; i++){
-//     if (i % 2 == 0){
-//         console.log(i);
+for (i = 1 ;i <= 10; i++){
+    if (i % 2 == 0){
+        console.log(i);
         
-//     }
-// }
+    }
+}
 
 // print sum of numbers from 1 to 5
 
-// for (i = 1;i<=5;i++){
-//     console.log();
+for (i = 1;i<=5;i++){
+    console.log();
     
-// }
+}
 
 
 // print numbers in reverse from 5 to 1 
 
-// for (i = 5;i>=1;i--){
-//     console.log(i);
+for (i = 5;i>=1;i--){
+    console.log(i);
     
-// }
+}
 
 // check if a number is positive or negative 
 
-// var num = -3;
+var num = -3;
 
-// if(num > 0){
-//     console.log("positive");
+if(num > 0){
+    console.log("positive");
     
-// }else if (num < 0){
-//     console.log("negative");
+}else if (num < 0){
+    console.log("negative");
     
-// }
+}
 
 
 // print first 3 multiples of 3
 
-// for( i = 1;i<=3;i++){
-//     console.log(i * 3);
+for( i = 1;i<=3;i++){
+    console.log(i * 3);
     
 
-// }
+}
 
 // print the largest of two numbers
 
-// var num1 = 5;
-// var num2 = 9;
+var num1 = 5;
+var num2 = 9;
 
-// if(num1>num2){
-//     console.log(num1 ,"is larger than num2");
+if(num1>num2){
+    console.log(num1 ,"is larger than num2");
     
-// }else if (num1<num2){
-//     console.log(num2 ,"is larger than num1");
+}else if (num1<num2){
+    console.log(num2 ,"is larger than num1");
     
-// }
+}
 
 // check if a number is even or odd 
 
-// var num = 7;
+var num = 7;
 
-// if (num % 2 == 0){
-//     console.log("number is even");
+if (num % 2 == 0){
+    console.log("number is even");
     
-// }else if (num % 2 == 1){
-//     console.log("number is odd");
+}else if (num % 2 == 1){
+    console.log("number is odd");
     
-// }
+}
 
 // print all numbers from 1 to n 
 
-// let n= 4;
+let n= 4;
 
-// for (var i=1; i<=n;i++){
-//     console.log(i);
+for (var i=1; i<=n;i++){
+    console.log(i);
     
-// }
+}
 
 // find the sum of two numbers
 
-// var num1 = 3;
-// var num2 = 8;
+var num1 = 3;
+var num2 = 8;
 
-// var sum = num1 + num2;
-// console.log(sum);
+var sum = num1 + num2;
+console.log(sum);
 
 // print first 5 natural numbers 
 
-// for (i = 1 ; i<=5;i++){
-//     console.log(i);
+for (i = 1 ; i<=5;i++){
+    console.log(i);
     
-// }
+}
 
 // check if a number is greater than 10 
 
-// var num = 12;
+var num = 12;
 
-// if (num > 10 ){
-//     console.log( "yes" )
+if (num > 10 ){
+    console.log( "yes" )
     
-// }else if (num < 10 ){
-//     console.log("no ");
+}else if (num < 10 ){
+    console.log("no ");
     
-// }
+}
 
 // print first four odd number 
 
-// for (i = 0; i <= 8;i++){
-//     if(i % 2 == 1){
-//         console.log(i);
+for (i = 0; i <= 8;i++){
+    if(i % 2 == 1){
+        console.log(i);
         
-//     }
-// }
+    }
+}
 
 // print the product of two numbers 
 
-// var num1 = 4;
-// var num2 = 5;
+var num1 = 4;
+var num2 = 5;
 
-// var product = num1 * num2;
-// console.log(product);
+var product = num1 * num2;
+console.log(product);
 
 // check if a number is zero 
 
-// var num = 0;
-// if (num === 0){
-//     console.log("yes");
+var num = 0;
+if (num === 0){
+    console.log("yes");
     
-// }else if (num != 0){
-//     console.log("no");
+}else if (num != 0){
+    console.log("no");
     
-// }
+}
 
 // print first 3 multiples of 5
 
-// for (i = 1 ; i<= 3;i++){
-//     console.log(i*5);
+for (i = 1 ; i<= 3;i++){
+    console.log(i*5);
     
-// }
+}
 
 // print sum of first 3 positive numbers 
 
@@ -152,27 +152,27 @@
 
 // check if a number is less than 100 
 
-// var num = 50;
-// if (num < 100){
-//     console.log("yes");
+var num = 50;
+if (num < 100){
+    console.log("yes");
     
-// }else if (num > 100){
-//     console.log("no");
+}else if (num > 100){
+    console.log("no");
     
-// }
+}
 // print the difference between two numbers 
 
-// var num1 = 10;
-// var num2 = 4;
+var num1 = 10;
+var num2 = 4;
 
-// var subtract = num1 - num2;
-// console.log(subtract);
+var subtract = num1 - num2;
+console.log(subtract);
 
 // print numbers from 1 to 10 
 
-// i = 1;
-// while (i<= 10 ){
-//     console.log(i);
-//     i++
+i = 1;
+while (i<= 10 ){
+    console.log(i);
+    i++
     
-// }
+}

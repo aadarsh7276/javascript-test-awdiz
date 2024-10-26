578859dff134df271fa8d98855f9b81d9ed15767
Aadarsh Yadav <152860259+aadarsh7276@users.noreply.github.com>
2024-10-26 15:56:19 +0530

    updates

---
+++ test2/app.js
@@ -0,0 +1,151 @@
+// print an array 
+
+// array = [1,2,3];
+
+// for (var i = 0 ; i <= array.length; i++){
+// console.log(array[i]);
+// }
+
+// print numbers from 1 to N 
+
+// N = 5;
+// for (i=1;i<=N;i++){
+//     console.log(i);
+    
+// }
+
+// find the sum of two numbers 
+
+// var a = 2;
+// var b = 3;
+
+// var sum = a +b;
+
+// console.log(sum);
+
+// check if a number is even or odd 
+
+// var num = 4;
+
+// if (num % 2 == 0){
+//     console.log("even");
+    
+// }else{
+//     console.log("odd");
+    
+// }
+
+// find the smallest of two numbers 
+
+// var a = 5;
+// var b = 3;
+
+// if (a < b){
+//     console.log(a);
+    
+// }else{
+//     console.log(b);
+    
+// }
+
+// check if a string is empty 
+
+// var string ="";
+// if (string == 0){
+//     console.log("empty");
+    
+// }else{
+//     console.log("not empty");
+    
+// }
+
+// count the elements in array 
+
+// array = [1,2,3,4];
+
+// console.log(array.length);
+
+// print first character of a string 
+
+// var string = "hello";
+
+// console.log(string[0]);
+
+
+// find the difference of two numbers 
+
+// var a = 10;
+// var b = 6;
+
+// var sub = a - b;
+// console.log(sub);
+
+// find the last element of array 
+
+// var array = [5,6,7,8];
+
+// console.log(array[3]);
+
+// convert a string to uppercase 
+
+// var string = "hello";
+
+// console.log(string.toUpperCase());
+
+// check if a number is positive  
+
+// var num = 7;
+// if (num>0){
+//     console.log("positive");
+
+    
+// }else{
+//     console.log("negative");
+    
+// }
+
+// find the length of the string 
+
+// var string = "apple";
+// console.log(string.length);
+
+// // check if all elements in an array are positive 
+// var array = [1,2,3];
+// // for (var i = 0;i<array.length;i++){
+//     if(array > 0){
+//         console.log("positive");
+        
+//     }else{
+//         console.log("negative");
+        
+//     }
+// }
+// 
+
+// print a string MUltiple times 
+
+// var string = "hii";
+// var repeat = 3;
+// console.log(string.repeat(repeat));
+ 
+// find the remainder when dividng two numbers 
+
+// var a = 10;
+// var b = 3;
+
+// var remainder = a % b;
+// console.log(remainder);
+
+// reverse a string 
+// var string =  "cat";
+
+// for (var i = 3;i>= string.length;i--){
+//     console.log(string[i]);
+    
+// }
+// print elements at even position in an array 
+// var array = [10,20,30,40,50];
+// for (var i = 0;i<=array.length;i+=2 ){
+//     console.log(array[i]);
+    
+// }
